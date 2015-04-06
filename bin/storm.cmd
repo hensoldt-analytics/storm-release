b5432382856820013f2ca0f6c93acd2b04aed1eb
Jungtaek Lim <kabhwan@gmail.com>
2015-04-06 23:53:31 +0900

    fix storm.cmd to evaluate 'shift' correctly with 'storm jar'

--- bin/storm.cmd
+++ bin/storm.cmd
@@ -84,11 +84,11 @@ if "%1" == "--service" (
     set CLASSPATH=%CLASSPATH%;%2
     set CLASS=%3
     set args=%4
-    shift
+    goto start
     :start
+    shift
     if [%4] == [] goto done
     set args=%args% %4
-    shift
     goto start
 
     :done
