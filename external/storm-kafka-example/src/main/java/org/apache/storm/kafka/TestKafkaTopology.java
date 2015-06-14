666b75eb165fc1ed3eb56da8fa227d14eef5b177
Parth Brahmbhatt <brahmbhatt.parth@gmail.com>
2015-06-13 20:15:53 -0700

    Make kafka example topology work with secure kafka. Changes to DynamicBroker so it can use the new zookeeper info.

--- external/storm-kafka-example/src/main/java/org/apache/storm/kafka/TestKafkaTopology.java
+++ external/storm-kafka-example/src/main/java/org/apache/storm/kafka/TestKafkaTopology.java
@@ -14,12 +14,16 @@ import backtype.storm.generated.StormTopology;
 
 public class TestKafkaTopology {
 
-    public static StormTopology buildTopology(String zkHosts) {
+    public static StormTopology buildTopology(String zkHosts, boolean isSecure) {
         TridentTopology topology = new TridentTopology();
 
         BrokerHosts zk = new ZkHosts(zkHosts);
 
         TridentKafkaConfig spoutConf = new TridentKafkaConfig(zk, "test-topic");
+
+        if(isSecure) {
+            spoutConf.securityProtocol = "PLAINTEXTSASL";
+        }
         spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
         OpaqueTridentKafkaSpout spout = new OpaqueTridentKafkaSpout(spoutConf);
 
@@ -33,15 +37,16 @@ public class TestKafkaTopology {
 
     public static void main(String[] args) throws Exception {
         Config conf = new Config();
+        boolean isSecure = Boolean.valueOf(args[1]);
 
         conf.setMaxSpoutPending(5);
-        if (args.length == 1) {
+        if (args.length == 2) {
             LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("kafka-test", conf, buildTopology(args[0]));
+            cluster.submitTopology("kafka-test", conf, buildTopology(args[0], isSecure));
 
-        } else if (args.length == 2) {
+        } else if (args.length == 3) {
             conf.setNumWorkers(1);
-            StormSubmitter.submitTopology(args[1], conf, buildTopology(args[0]));
+            StormSubmitter.submitTopology(args[2], conf, buildTopology(args[0], isSecure));
         } else {
             System.out.println("Usage: TestKafkaTopology <zookeeper hosts> [topology name]");
             System.out.println("If [topology name] is specified, the topology will run on a remote cluster.");
