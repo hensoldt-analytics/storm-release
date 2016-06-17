ba1dd8bbbe3c638ab43baca3c073e87817ce3301
Arun Mahadevan <arunm@apache.org>
2016-06-17 12:25:09 +0530

    EAR-3734: Spawn thread in HBaseSecurityUtil.login to periodically relogin from keytab

--- external/storm-hbase/src/main/java/org/apache/storm/hbase/security/HBaseSecurityUtil.java
+++ external/storm-hbase/src/main/java/org/apache/storm/hbase/security/HBaseSecurityUtil.java
@@ -25,8 +25,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static backtype.storm.Config.TOPOLOGY_AUTO_CREDENTIALS;
 
@@ -38,6 +43,9 @@ import static backtype.storm.Config.TOPOLOGY_AUTO_CREDENTIALS;
  */
 public class HBaseSecurityUtil {
     private static final Logger LOG = LoggerFactory.getLogger(HBaseSecurityUtil.class);
+    private static final long KRB_RELOGIN_INTERVAL_MS = 5 * 60 * 1000; // 5 mins
+    private static Map<UserGroupInformation, ExecutorService> renewThreads = new HashMap<>();
+
 
     public static final String STORM_KEYTAB_FILE_KEY = "storm.keytab.file";
     public static final String STORM_USER_NAME_KEY = "storm.kerberos.principal";
@@ -63,12 +71,59 @@ public class HBaseSecurityUtil {
                         }
                         legacyProvider.login(STORM_KEYTAB_FILE_KEY, STORM_USER_NAME_KEY,
                                 InetAddress.getLocalHost().getCanonicalHostName());
+                        // spawn a thread to periodically re-login in secure mode
+                        UserGroupInformation ugi = legacyProvider.getCurrent().getUGI();
+                        spawnReLoginThread(ugi);
                     }
                 }
             }
             return legacyProvider;
         } else {
             return UserProvider.instantiate(hbaseConfig);
+        }
+    }
+
+
+    private synchronized static void spawnReLoginThread(final UserGroupInformation ugi) {
+        if (!renewThreads.containsKey(ugi)) {
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        LOG.debug("HBaseUtils invoking re-login from keytab for ugi {}", ugi);
+                        ugi.checkTGTAndReloginFromKeytab();
+                    } catch (Throwable th) {
+                        LOG.error("Got error while trying to relogin from keytab", th);
+                    }
+                }
+            };
+
+            LOG.debug("Adding re-login task for ugi {}", ugi);
+            ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+            executorService.scheduleAtFixedRate(task, KRB_RELOGIN_INTERVAL_MS, KRB_RELOGIN_INTERVAL_MS, TimeUnit.MILLISECONDS);
+            renewThreads.put(ugi, executorService);
+        }
+    }
+
+    public synchronized static void killReLoginThread(final UserGroupInformation ugi) {
+        LOG.debug("Killing re-login task for ugi {}", ugi);
+        if (renewThreads.containsKey(ugi)) {
+            doKillReLoginThread(renewThreads.get(ugi));
+            renewThreads.remove(ugi);
+        } else {
+            LOG.warn("No re-login thread is running for ugi {}", ugi);
+        }
+    }
+
+    private static void doKillReLoginThread(ExecutorService executorService) {
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(2, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException ie) {
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
         }
     }
 }
